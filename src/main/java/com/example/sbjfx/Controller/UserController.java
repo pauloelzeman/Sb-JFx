da1bf5708b1f7f4f5ca06be52f2c8b2bf25508f9
Paulo Elzeman <paulo.elzeman@finxapp.com.br>
2023-12-27 19:02:45 -0300

    Commit - index.html

--- src/main/java/com/example/sbjfx/Controller/UserController.java
+++ src/main/java/com/example/sbjfx/Controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping("/")
+    @GetMapping("/login")
     public String GetUser()
     {
         return "GetUser";
--- src/main/java/com/example/sbjfx/SbJFxApplication.java
+++ src/main/java/com/example/sbjfx/SbJFxApplication.java
@@ -1,13 +1,34 @@
 package com.example.sbjfx;
 
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.URL;
+
 
 @SpringBootApplication
-public class SbJFxApplication {
+public class SbJFxApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SbJFxApplication.class, args);
+     //   launch(args);
     }
 
+/*
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       URL arquivoFXML = getClass().getResource("Login.fxml");
+       GridPane raiz = FXMLLoader.load(arquivoFXML);
+       Scene cena = new Scene(raiz, 350, 350);
+
+       primaryStage.setResizable(false);
+       primaryStage.setTitle("Tela de Login");
+       primaryStage.setScene(cena);
+       primaryStage.show();
+    }
+    */
 }
